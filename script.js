3aad462e385c4e5664f362bbad7e64051a0794f9
Rafi darussalam <Alghifarimaulaabi@gmail.com>
2025-04-04 20:31:21 +0700

    Up

---
+++ script.js
@@ -0,0 +1,42 @@
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function() {
+        menu.toggle(); 
+    });
+    menu.click(function() {
+        menu.toggle(); 
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) { 
+        klikMenu();
+    }
+});
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-250px";
+    } else {
+        sidebar.style.left = "0px";
+    }
+}
+
+
+//check lebar
+$(window).resize(function(){
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display","block");
+        //display block
+    }else{
+        menu.css("display","none");
+    }
+    klikMenu();
+});
+
